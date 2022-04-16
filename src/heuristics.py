4590cb30ae638a14208b7a32c981e19bcc99e13f
Bruno L. Carli <brunolcarli@gmail.com>
2022-04-16 16:00:57 -0300

    ADD heuristics module

---
+++ src/heuristics.py
@@ -0,0 +1,100 @@
+
+highlands_distances = {
+    'Case Town': {
+        'Case Town': 0,
+        'Alexandrea': 100,
+        'Por Town': 322
+    },
+    'Alexandrea': {
+        'Alexandrea': 0,
+        'Case Town': 100
+    },
+    'Por Town': {
+        'Por Town': 0,
+        'Case Town': 322,
+        'Fisherman House': 82,
+        'Loria': 476,
+        'Sonospla': 790
+    },
+    'Fisherman House': {
+        'Fisherman House': 0,
+        'Island Tower': 56,
+        'Por Town': 82
+    },
+    'Island Tower': {
+        'Island Tower': 0,
+        'Fisherman House': 56
+    },
+    'Loria': {
+        'Loria': 0,
+        'Por Town': 476,
+        'Sonospla': 54,
+        'Goblin Dew': 233
+    },
+    'Sonospla': {
+        'Sonospla': 0,
+        'Loria': 54,
+        'Por Town': 790,
+        'Caravan': 87,
+        'Desert Fortress': 80
+    },
+    'Caravan': {
+        'Caravan': 0,
+        'Sonospla': 87,
+        'Desert Fortress': 30,        
+    },
+    'Desert Fortress': {
+        'Desert Fortress': 0,
+        'Caravan': 30,
+        'Arcadia': 88,
+        'Malma City': 78,
+        'Sonospla': 80,
+        'Vault Gisley': 300
+    },
+    'Arcadia': {
+        'Arcadia': 0,
+        'Desert Fortress': 88,
+        'Malma City': 13,
+    },
+    'Malma City': {
+        'Malma City': 0,
+        'Arcadia': 13,
+        'Desert Fortress': 78,
+        'Vault Gisley': 234
+    },
+    'Goblin Dew': {
+        'Goblin Dew': 0,
+        'Loria': 233,
+        'Lokiana Town': 23,
+        'Elfenheim': 54,
+    },
+    'Lokiana Town': {
+        'Lokiana Town': 0,
+        'Goblin Dew': 23,
+        'Elfenheim': 10,
+        'Gomorra': 120
+    },
+    'Elfenheim': {
+        'Elfenheim': 0,
+        'Lokiana Town': 10,
+        'Goblin Dew': 54,
+        'Gomorra': 100
+    },
+    'Gomorra': {
+        'Gomorra': 0,
+        'Lokiana Town': 10,
+        'Elfenheim': 100,
+        'Orcs Camp': 30
+    },
+    'Orcs Camp': {
+        'Orcs Camp': 0,
+        'Gomorra': 30,
+        'Vault Gisley': 8
+    },
+    'Vault Gisley': {
+        'Vault Gisley': 0,
+        'Orcs Camp': 8,
+        'Desert Fortress': 300,
+        'Malma City': 234
+    }
+}
